fee15aa13fcd82d453c96c5e392b93e503f18cbf
Yasel Glez <lesaywd@gmail.com>
2023-05-23 12:25:35 -0400

    creando metodo get y add serv a moule

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import{HttpClient}from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
 
-  constructor() { }
+export class UserService {
+private url='http://localhost:3000/users';
+  constructor(private http:HttpClient) { }
+  
+  getUsers(){
+    this.http.get(this.url).subscribe(data => {
+      console.log(data);
+    });
+    console.log("Esto se ejecutará antes que el console log de arriba");
+  }
+  
 }

703f812fde4ffce5c5bdee080215138d018e4bc3
Yasel Glez <lesaywd@gmail.com>
2023-05-26 14:29:43 -0400

    add mterial and UI usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserService } from './services/user.service';
 import { UserComponent } from './components/user/user.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { UserComponent } from './components/user/user.component';
     UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+
+@NgModule({
+    exports:[
+        MatButtonModule,
+        MatCardModule,
+        MatIconModule
+    ]
+})
+
+export class MaterialModule { }
